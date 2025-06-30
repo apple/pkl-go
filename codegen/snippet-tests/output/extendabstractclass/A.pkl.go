26f823f5470872c94dff46d39310144c42f85d4b
Dovy Bartkevicius <github@dovys.com>
2025-06-30 22:01:28 +0100

    Fix abstract class extending another abstract class.
    I've ran into a situation where I have:
    
    ```pkl
    import "pkg.pkl"
    
    abstract class A {
      field: pkg.Type
    }
    
    abstract class B extends A {
      secondField: String
    }
    
    class Concrete extends B {
      secondField = "defined"
      field { ... }
    }
    ```
    
    This would result in 3 class files being generated, with B.pkl.go:
    
    ```go
    package mypackage
    
    import ".../pkg"
    
    type B interface {
    	SomeField string
    }
    ```
    
    Since both A and B are abstract we generate interfaces and B should not have any imports as the pkg field type is defined in A.

--- codegen/snippet-tests/output/extendabstractclass/A.pkl.go
+++ codegen/snippet-tests/output/extendabstractclass/A.pkl.go
@@ -1,6 +1,10 @@
 // Code generated from Pkl module `ExtendsAbstractClass`. DO NOT EDIT.
 package extendabstractclass
 
+import "github.com/apple/pkl-go/codegen/snippet-tests/output/support/lib3"
+
 type A interface {
 	GetB() string
+
+	GetC() lib3.GoGoGo
 }
