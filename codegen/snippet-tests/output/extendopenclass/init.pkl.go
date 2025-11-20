4156210a64669e6cb979043ba290654d92dd3fff
Jen Basch <jbasch@apple.com>
2025-11-20 14:47:10 -0800

    Ensure registration order is stable in generated init.pkl.go files

--- codegen/snippet-tests/output/extendopenclass/init.pkl.go
+++ codegen/snippet-tests/output/extendopenclass/init.pkl.go
@@ -6,6 +6,6 @@ import "github.com/apple/pkl-go/pkl"
 func init() {
 	pkl.RegisterStrictMapping("ExtendingOpenClass", ExtendingOpenClass{})
 	pkl.RegisterStrictMapping("ExtendingOpenClass#MyClass", MyClassImpl{})
-	pkl.RegisterStrictMapping("ExtendingOpenClass#MyOpenClass", MyOpenClassImpl{})
 	pkl.RegisterStrictMapping("ExtendingOpenClass#MyClass2", MyClass2Impl{})
+	pkl.RegisterStrictMapping("ExtendingOpenClass#MyOpenClass", MyOpenClassImpl{})
 }
