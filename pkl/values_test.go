d474caf45e22b0b5125c13d1797cd6852a16d278
NawafSwe <nawaf.jr9@hotmail.com>
2026-03-15 04:08:34 +0300

    chore: implement data size unmarshal binary

--- pkl/values_test.go
+++ pkl/values_test.go
@@ -56,7 +56,35 @@ func TestDataSize_String(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
 			assert.Equal(t, test.expected, test.input.String())
+		})
+	}
+}
+
+func TestDataSizeUnit_String(t *testing.T) {
+	tests := []struct {
+		name string
+		unit DataSizeUnit
+		want string
+	}{
+		{name: "byte", unit: Bytes, want: "b"},
+		{name: "kilobyte", unit: Kilobytes, want: "kb"},
+		{name: "kibibyte", unit: Kibibytes, want: "kib"},
+		{name: "megabyte", unit: Megabytes, want: "mb"},
+		{name: "mebibyte", unit: Mebibytes, want: "mib"},
+		{name: "gigabyte", unit: Gigabytes, want: "gb"},
+		{name: "gibibyte", unit: Gibibytes, want: "gib"},
+		{name: "terabyte", unit: Terabytes, want: "tb"},
+		{name: "tebibyte", unit: Tebibytes, want: "tib"},
+		{name: "petabyte", unit: Petabytes, want: "pb"},
+		{name: "pebibyte", unit: Pebibytes, want: "pib"},
+		{name: "invalid", unit: DataSizeUnit(-1), want: "<invalid>"},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, test.want, test.unit.String())
 		})
 	}
 }
@@ -103,25 +131,25 @@ func TestDuration_String(t *testing.T) {
 		d    Duration
 		want string
 	}{
-		"should successfully return valid seconds string": {
+		"should successfully return valid second string": {
 			d: Duration{
 				Unit: Second,
 			},
 			want: "s",
 		},
-		"should successfully return valid milliseconds string": {
+		"should successfully return valid millisecond string": {
 			d: Duration{
 				Unit: Millisecond,
 			},
 			want: "ms",
 		},
-		"should successfully return valid nanoseconds string": {
+		"should successfully return valid nanosecond string": {
 			d: Duration{
 				Unit: Nanosecond,
 			},
 			want: "ns",
 		},
-		"should successfully return valid microseconds string": {
+		"should successfully return valid microsecond string": {
 			d: Duration{
 				Unit: Microsecond,
 			},
@@ -163,7 +191,7 @@ func TestDuration_String(t *testing.T) {
 }
 
 func TestDuration_GoDuration(t *testing.T) {
-	t.Run("should successfully convert to GoDuration in seconds", func(t *testing.T) {
+	t.Run("should successfully convert to GoDuration in second", func(t *testing.T) {
 		t.Parallel()
 		d := Duration{
 			Value: 1,
@@ -171,4 +199,87 @@ func TestDuration_GoDuration(t *testing.T) {
 		}
 		assert.Equal(t, d.GoDuration(), 1*time.Second)
 	})
+}
+
+func TestDuration_UnmarshalBinary(t *testing.T) {
+	tests := map[string]struct {
+		input       []byte
+		want        DurationUnit
+		expectedErr error
+	}{
+		"should successfully unmarshal binary from nanosecond": {
+			input: []byte("ns"),
+			want:  Nanosecond,
+		},
+		"should successfully unmarshal binary from  microsecond": {
+			input: []byte("us"),
+			want:  Microsecond,
+		},
+		"should successfully unmarshal binary from  millisecond": {
+			input: []byte("ms"),
+			want:  Millisecond,
+		},
+		"should successfully unmarshal binary from  second": {
+			input: []byte("s"),
+			want:  Second,
+		},
+		"should successfully unmarshal binary from  minute": {
+			input: []byte("min"),
+			want:  Minute,
+		},
+		"should successfully unmarshal binary from  hour": {
+			input: []byte("h"),
+			want:  Hour,
+		},
+		"should successfully unmarshal binary from  day": {
+			input: []byte("d"),
+			want:  Day,
+		},
+		"should fail unmarshal binary from unknown field": {
+			input:       []byte("unknown"),
+			expectedErr: fmt.Errorf("unrecognized Duration unit: `unknown`"),
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			var unit DurationUnit
+			err := unit.UnmarshalBinary(tc.input)
+
+			assert.Equal(t, tc.want, unit)
+			assert.Equal(t, tc.expectedErr, err)
+		})
+	}
+}
+
+func TestDataSizeUnit_UnmarshalBinary(t *testing.T) {
+	tests := []struct {
+		name        string
+		input       []byte
+		want        DataSizeUnit
+		expectedErr error
+	}{
+		{name: "byte", input: []byte("b"), want: Bytes},
+		{name: "kilobyte", input: []byte("kb"), want: Kilobytes},
+		{name: "kibibyte", input: []byte("kib"), want: Kibibytes},
+		{name: "megabyte", input: []byte("mb"), want: Megabytes},
+		{name: "mebibyte", input: []byte("mib"), want: Mebibytes},
+		{name: "gigabyte", input: []byte("gb"), want: Gigabytes},
+		{name: "gibibyte", input: []byte("gib"), want: Gibibytes},
+		{name: "terabyte", input: []byte("tb"), want: Terabytes},
+		{name: "tebibyte", input: []byte("tib"), want: Tebibytes},
+		{name: "petabyte", input: []byte("pb"), want: Petabytes},
+		{name: "pebibyte", input: []byte("pib"), want: Pebibytes},
+		{name: "unknown", input: []byte("unknown"), expectedErr: fmt.Errorf("unrecognized DataSize unit: `unknown`")},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			var unit DataSizeUnit
+			err := unit.UnmarshalBinary(test.input)
+
+			assert.Equal(t, test.want, unit)
+			assert.Equal(t, test.expectedErr, err)
+		})
+	}
 }
